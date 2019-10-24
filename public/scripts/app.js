0e46f98b14e1c938a07fb9edbd5f9df466b1b142
Max Stevenson <maxstevenson@msn.com>
2019-10-24 15:11:28 +0100

    Second template completed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,47 @@
 'use strict';
 
 var template = React.createElement(
-  'p',
-  null,
-  'Does this change?'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Some Info'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Does this change?'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'I\'m a list item'
+        )
+    )
+);
+
+var secondTemplate = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Max Stevenson'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 28'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: London'
+    )
 );
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(secondTemplate, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
-var template = <p>Does this change?</p>;
+var template = (
+    <div> 
+        <h1>Some Info</h1> 
+        <p>Does this change?</p>
+        <ol>
+            <li>I'm a list item</li>
+        </ol>
+    </div>
+);
+
+var secondTemplate = (
+    <div>
+        <h1>Max Stevenson</h1>
+        <p>Age: 28</p>
+        <p>Location: London</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(secondTemplate, appRoot);
