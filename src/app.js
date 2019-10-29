456a52ae109f8ee9d044e2ee30090d36279173d9
Max Stevenson <maxstevenson@msn.com>
2019-10-29 19:42:59 +0000

    Changed to correct spacing size

--- src/app.js
+++ src/app.js
@@ -1,52 +1,51 @@
 class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Indecision App</h1>
-                <h2>Put your life in the hands of a computer</h2>
-            
-            </div>
-        );
-    };
+	render() {
+		return (
+			<div>
+				<h1>Indecision App</h1>
+				<h2>Put your life in the hands of a computer</h2>
+			</div>
+		);
+	};
 };
 
 class Action extends React.Component {
-    render() {
-        return (
-            <div>
-                <button>What should I do?</button>
-            </div>
-        );
-    };
+	render() {
+		return (
+			<div>
+				<button>What should I do?</button>
+			</div>
+		);
+	};
 };
 
 class Options extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Here are your options: </p>
-            </div>
-        );
-    };
+	render() {
+		return (
+			<div>
+				<p>Here are your options: </p>
+			</div>
+		);
+	};
 };
 
 class AddOption extends React.Component {
-    render() {
-        return (
-            <div>
-                <button>Add Option</button>
-            </div>
-        );
-    };
+	render() {
+		return (
+			<div>
+				<button>Add Option</button>
+			</div>
+		);
+	};
 };
 
 const jsx = (
-    <div>
-        <Header></Header>
-        <Action></Action>
-        <Options></Options>
-        <AddOption></AddOption>
-    </div>
+	<div>
+		<Header></Header>
+		<Action></Action>
+		<Options></Options>
+		<AddOption></AddOption>
+	</div>
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
