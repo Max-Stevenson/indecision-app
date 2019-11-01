a8a4014a4963151598247045549ea28c059af156
Max Stevenson <maxstevenson@msn.com>
2019-11-01 12:35:55 +0000

    Component state persisted in localstorage

--- src/app.js
+++ src/app.js
@@ -10,6 +10,19 @@ class IndecisionApp extends React.Component {
 		};
 	};
 
+	componentDidMount() {
+		const json = localStorage.getItem('options');
+		const options = JSON.parse(json);
+		this.setState(() => ({options}));
+	};
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.options.length !== this.state.options.length) {
+			const json = JSON.stringify(this.state.options);
+			localStorage.setItem('options', json);
+		};
+	};
+
 	handleDeleteOptions() {
 		this.setState(() => ({options: []}));
 	};
