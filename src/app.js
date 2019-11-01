67fbfbdea188f414e2a0e31d6fefaf74e23921be
Max Stevenson <maxstevenson@msn.com>
2019-11-01 10:47:33 +0000

    Refactor of setState calls to be less verbose

--- src/app.js
+++ src/app.js
@@ -10,17 +10,12 @@ class IndecisionApp extends React.Component {
 	};
 
 	handleDeleteOptions() {
-		this.setState(() => {
-			return {
-				options: []
-			};
-		});
+		this.setState(() => ({options: []}));
 	};
 
 	handleClick() {
 		const optionIdx = Math.floor(Math.random() * this.state.options.length);
 		console.log(this.state.options[optionIdx]);
-		
 	};
 
 	handleAddOption(option) {
@@ -30,11 +25,9 @@ class IndecisionApp extends React.Component {
 			return 'This option already exists';
 		};
 
-		this.setState((prevState) => {
-			return {
-				options: prevState.options.concat(option)
-			};
-		});
+		this.setState((prevState) => ({
+			options: prevState.options.concat(option)
+		}));
 	};
 
 	render() {
@@ -122,11 +115,7 @@ class AddOption extends React.Component {
 		const option = event.target.elements.option.value.trim();
 		const error = this.props.handleAddOption(option);				
 		
-		this.setState(() => {
-			return {
-				error
-			};
-		});
+		this.setState(() => ({error}));
 	};
 
 	render() {
