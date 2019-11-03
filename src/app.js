d725b2a31aa154058dfe233e5368c9533aad8c7c
Max Stevenson <maxstevenson@msn.com>
2019-11-03 16:40:52 +0000

    Extracted out AddOption to own component file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AddOption from './components/AddOption.js';
 
 class IndecisionApp extends React.Component {
 	constructor(props) {
@@ -140,41 +141,6 @@ const Action = (props) => {
 			</button>
 		</div>
 	);
-};
-
-class AddOption extends React.Component {
-
-	constructor(props) {
-		super(props);
-		this.handleAddOption = this.handleAddOption.bind(this);
-		this.state = {
-			error: undefined
-		};
-	};
-
-	handleAddOption(event) {
-		event.preventDefault();
-		const option = event.target.elements.option.value.trim();
-		const error = this.props.handleAddOption(option);
-
-		this.setState(() => ({ error }));
-
-		if (!error) {
-			event.target.elements.option.value = '';
-		};
-	};
-
-	render() {
-		return (
-			<div>
-				{this.state.error && <p>{this.state.error}</p>}
-				<form onSubmit={this.handleAddOption}>
-					<input type='text' name='option'></input>
-					<button>Add Option</button>
-				</form>
-			</div>
-		);
-	};
 };
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
