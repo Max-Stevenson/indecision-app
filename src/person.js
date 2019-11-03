8797d5cc2548e9a8389628e0e8df55c7eb815272
Max Stevenson <maxstevenson@msn.com>
2019-11-03 16:19:41 +0000

    Indecision App restored

--- src/person.js
+++
@@ -1,6 +0,0 @@
-const canDrink = (age) => age >= 18 ? true : false;
-const isAdult = (age) => age >= 18 ? true : false;
-
-
-
-export {canDrink, isAdult}
--- src/utils.js
+++
@@ -1,8 +0,0 @@
-console.log('utils is running');
-
-const square = (x) => { return x * x };
-const add = (x, y) => x + y;
-
-const subtract = (x, y) => x - y;
-
-export { square, add, subtract as default };
