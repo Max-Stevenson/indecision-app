08ed74a52e37989606625352896e58ff04098049
Max Stevenson <maxstevenson@msn.com>
2019-11-03 15:35:08 +0000

    Validator installed

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
-import { square, add } from './utils';
+import subtract, { square, add } from './utils';
 import { isAdult, canDrink } from './person';
 
 console.log(square(5));
 console.log(add(5, 5));
+console.log(subtract(10, 5));
 
 console.log(canDrink(16));
 console.log(isAdult(18));
-
-
-
 
 console.log('webpackApp.js running fine still');
--- src/utils.js
+++ src/utils.js
@@ -3,4 +3,6 @@ console.log('utils is running');
 const square = (x) => { return x * x };
 const add = (x, y) => x + y;
 
-export { square, add };
+const subtract = (x, y) => x - y;
+
+export { square, add, subtract as default };
