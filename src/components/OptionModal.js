73500b36c25564a674c38944470820dd296b8d14
Max Stevenson <maxstevenson@msn.com>
2019-11-08 09:57:04 +0000

    Refactor to use implicit return in component classes

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,18 +2,16 @@ import React from 'react';
 import Modal from 'react-modal';
 Modal.setAppElement('#app');
 
-const OptionModal = (props) => {
-  return (
-    <Modal
+const OptionModal = (props) => (
+  <Modal
     isOpen={!!props.selectedOption}
     onRequestClose={props.clearSelectedOption}
     contentLabel="ShowSelectedOption"
-    >
+  >
     <h3>Selected Option</h3>
     {props.selectedOption && <p>{props.selectedOption}</p>}
     <button onClick={props.clearSelectedOption}>Ok</button>
   </Modal>
-  );
-};
+);
 
 export default OptionModal;
